6b1f6b908895f611debe5f18332aec9bbeb3a9ae
Yang Bo <atryyang@thoughtworks.com>
2018-06-24 02:22:37 +0000

    Suppress "Authentication failure" error

--- docker-run-su.sh
+++ docker-run-su.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 nvidia-docker run \
     --volume /etc/passwd:/etc/passwd:ro \
+    --volume /etc/shadow:/etc/shadow:ro \
     --volume "$HOME:$HOME" \
     --volume "$PWD:/mnt/project-root" \
     --workdir /mnt/project-root \
